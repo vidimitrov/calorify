f6b6390b8328516992bafcc066f0ba0b6df40083
Veselin Dimitrov <vidimi7rov@gmail.com>
2018-11-03 15:59:43 +0200

    Fix a route import error and add a new testcase for the health-check endpoint

--- server/src/api/health-check/get.spec.ts
+++ server/src/api/health-check/get.spec.ts
@@ -1,16 +1,26 @@
+import request from 'supertest';
+import app from '../../app';
 import get from './get';
 import { ENV } from '../../../config';
 
 describe('Health check endpoint', () => {
-  it('GET / should return status code 200', async () => {
+  it('get middleware should attach status code 200 to ctx', async () => {
     const ctx: any = {};
     await get(ctx);
     expect(ctx.status).toBe(200);
   });
 
-  it('GET / should return proper body', async () => {
+  it('get middleware should attach proper body to ctx', async () => {
     const ctx: any = {};
     await get(ctx);
     expect(ctx.body).toBe(`Calorify API is up and running in ${ENV} environment...`);
+  });
+
+  it('GET / should return healthy response', async () => {
+    const response = await request(app.callback()).get('/');
+    expect(response.body).toEqual({});
+    expect(response.type).toEqual('text/plain');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe(`Calorify API is up and running in ${ENV} environment...`);
   });
 });
