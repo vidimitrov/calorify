6abe0be6c07db3ae8827070553f0d92300126dc6
Veselin Dimitrov <vidimi7rov@gmail.com>
2018-11-04 01:55:12 +0200

    Add routes for the Users API

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,7 +1,9 @@
 import status from './health-check';
 import auth from './auth';
+import users from './users';
 
 export default {
   status,
   auth,
+  users,
 };
--- server/src/router.ts
+++ server/src/router.ts
@@ -1,5 +1,6 @@
 import Router from 'koa-router';
 import api from './api';
+import protect from './middlewares/protect';
 
 const router = new Router();
 
@@ -10,5 +11,12 @@ router.get('/', api.status.get);
 router.post('/api/auth/login', api.auth.login);
 router.post('/api/auth/signup', api.auth.signup);
 router.get('/api/auth/logout', api.auth.logout);
+
+// Users
+router.get('/api/users', protect, api.users.list);
+router.get('/api/users/:userId', protect, api.users.findById);
+router.post('/api/users', protect, api.users.create);
+router.patch('/api/users/:userId', protect, api.users.update);
+router.delete('/api/users/:userId', protect, api.users.remove);
 
 export default router;
