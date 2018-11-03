d64766174d25a579996367b9426b9dcdc1378c8e
Veselin Dimitrov <vidimi7rov@gmail.com>
2018-11-04 01:45:29 +0200

    Add access control constants

---
+++ server/src/constants/ac.ts
@@ -0,0 +1,13 @@
+// Resources
+export const USER = 'user';
+export const MEAL = 'meal';
+
+// Actions
+export const CREATE_ANY = 'createAny';
+export const CREATE_OWN = 'createOwn';
+export const READ_ANY = 'readAny';
+export const READ_OWN = 'readOwn';
+export const UPDATE_ANY = 'updateAny';
+export const UPDATE_OWN = 'updateOwn';
+export const DELETE_ANY = 'deleteAny';
+export const DELETE_OWN = 'deleteOwn';
