bef4b835bb8d8a0e78486de7ecda6e08f1376915
Veselin Dimitrov <vidimi7rov@gmail.com>
2018-11-03 17:19:33 +0200

    Add migration for creating the users table

---
+++ server/db/migrations/20181103163130_create_user.ts
@@ -0,0 +1,24 @@
+import { USERS_TABLE } from '../constants';
+
+export const up = (knex) => {
+  return knex.raw(`
+    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+    CREATE TABLE ${USERS_TABLE} (
+      id uuid PRIMARY KEY DEFAULT uuid_generate_v4() UNIQUE,
+      role varchar(50),
+      password text NOT NULL,
+      name varchar(200) NOT NULL,
+      email varchar(100) NOT NULL,
+      provider varchar(50) DEFAULT 'local',
+      deleted boolean DEFAULT false,
+      created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+      updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+  );
+  `);
+};
+
+export const down = (knex) => {
+  return knex.raw(`
+    DROP TABLE ${USERS_TABLE} CASCADE;
+  `);
+};
