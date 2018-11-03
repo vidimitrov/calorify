451af7e910333720759864a38d4458eb534d6661
Veselin Dimitrov <vidimi7rov@gmail.com>
2018-11-03 17:19:45 +0200

    Add migration to create the meals table

---
+++ server/db/migrations/20181103163135_create_meals.ts
@@ -0,0 +1,24 @@
+import { MEALS_TABLE, USERS_TABLE } from '../constants';
+
+export const up = (knex) => {
+  return knex.raw(`
+    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+    CREATE TABLE ${MEALS_TABLE} (
+      id uuid PRIMARY KEY DEFAULT uuid_generate_v4() UNIQUE,
+      user_id uuid NOT NULL REFERENCES ${USERS_TABLE}(id),
+      text text NOT NULL,
+      date DATE NOT NULL DEFAULT NOW(),
+      time TIME NOT NULL DEFAULT NOW(),
+      number_of_calories integer,
+      deleted boolean DEFAULT false,
+      created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+      updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+  );
+  `);
+};
+
+export const down = (knex) => {
+  return knex.raw(`
+    DROP TABLE ${MEALS_TABLE} CASCADE;
+  `);
+};
