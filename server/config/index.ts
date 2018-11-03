79f9efa5a5dc4d5f92c2c0e467b557ffe4a39d96
Veselin Dimitrov <vidimi7rov@gmail.com>
2018-11-03 16:11:02 +0200

    Add PostgreSQL related environment configuration

--- server/config/index.ts
+++ server/config/index.ts
@@ -6,3 +6,13 @@ const cfg = require(`./env/${ENV}`);
 export const HOST = process.env.HOST || cfg['HOST'] || 'localhost';
 export const PORT = process.env.PORT || cfg['PORT'] || 3000;
 export const API_SECRET = process.env.API_SECRET || cfg['API_SECRET'];
+
+// PostgreSQL
+export const PG_URI =
+  process.env.PG_URI || cfg['PG_URI'];
+
+export const PG_POOL_MIN =
+  process.env.PG_POOL_MIN || cfg['PG_POOL_MIN'] || 0;
+
+export const PG_POOL_MAX =
+  process.env.PG_POOL_MAX || cfg['PG_POOL_MAX'] || 2;
