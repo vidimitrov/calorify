cf3a319577e5f8a713b537249928bb5bbe4d014d
Veselin Dimitrov <vidimi7rov@gmail.com>
2018-11-03 23:35:07 +0200

    Add passport configuration and use the local strategy

---
+++ server/config/passport/index.ts
@@ -0,0 +1,18 @@
+import passport from 'koa-passport';
+import local from './strategies/local';
+import User from '../../src/models/user/User';
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+passport.use(local);
