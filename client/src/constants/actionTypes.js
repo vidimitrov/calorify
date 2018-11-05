14e764e50a82f328b63d2a63bfc793b2e5342f99
Veselin Dimitrov <vidimi7rov@gmail.com>
2018-11-05 17:12:42 +0200

    Add authentication based action types constants

---
+++ client/src/constants/actionTypes.js
@@ -0,0 +1,8 @@
+// Authentication
+export const STORE_USER_DATA = 'STORE_USER_DATA';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
+export const SIGNUP_FAILURE = 'SIGNUP_FAILURE';
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
+export const LOGOUT_FAILURE = 'LOGOUT_FAILURE'
