1b88fb80178033ebe6a34db6a215294318d11627
Veselin Dimitrov <vidimi7rov@gmail.com>
2018-11-07 00:07:53 +0200

    Add AppBar to UserSettings view

--- client/src/containers/dashboard/UserSettings.js
+++ client/src/containers/dashboard/UserSettings.js
@@ -1,5 +1,72 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Redirect } from '@reach/router';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import ArrowBack from '@material-ui/icons/ArrowBack';
+import Logo from '../../components/dashboard/Logo';
+import Wrapper from '../../components/dashboard/Wrapper';
+import CustomTypography from '../../components/dashboard/CustomTypography';
+import CustomIconButton from '../../components/dashboard/CustomIconButton';
+import logo from '../../assets/img/logo.png';
 
-export const UserSettings = () => (<div>User settings view</div>);
+export class UserSettings extends React.Component {
+  constructor(props) {
+    super(props);
 
-export default UserSettings;
+    this.state = {
+    };
+  }
+
+  render() {
+    const {
+      user,
+    } = this.props;
+    const { navigate } = this.props;
+
+    if (!user) {
+      return (
+        <Redirect to="/auth/login" />
+      );
+    }
+
+    return (
+      <Wrapper>
+        <AppBar position="static">
+          <Toolbar>
+            <CustomIconButton color="secondary" aria-label="Menu">
+              <ArrowBack onClick={() => navigate('/')} />
+            </CustomIconButton>
+            <Logo src={logo} alt="logo" />
+            <CustomTypography variant="h6" color="secondary">
+              User Settings
+            </CustomTypography>
+          </Toolbar>
+        </AppBar>
+      </Wrapper>
+    );
+  }
+}
+
+UserSettings.propTypes = {
+  user: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    role: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    provider: PropTypes.string.isRequired,
+    daily_calories_limit: PropTypes.number.isRequired,
+  }),
+  navigate: PropTypes.func.isRequired,
+};
+
+UserSettings.defaultProps = {
+  user: null,
+};
+
+const mapStateToProps = state => ({
+  user: state.auth.user,
+});
+
+export default connect(mapStateToProps)(UserSettings);
