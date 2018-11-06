ff2148a41b778efe922cc88480b53410e198588c
Veselin Dimitrov <vidimi7rov@gmail.com>
2018-11-06 03:02:30 +0200

    Finalize the UI of the authentication views

--- client/src/containers/authentication/Login.js
+++ client/src/containers/authentication/Login.js
@@ -1,5 +1,43 @@
 import React from 'react';
+// import PropsTypes from 'prop-types';
+import logo from '../../assets/img/logo.png';
+import Wrapper from '../../components/authentication/Wrapper';
+import FormHeader from '../../components/authentication/FormHeader';
+import FormContent from '../../components/authentication/FormContent';
+import FormFooter from '../../components/authentication/FormFooter';
+import SecondaryAction from '../../components/authentication/SecondaryAction';
+import Input from '../../components/authentication/Input';
+import SubmitButton from '../../components/authentication/SubmitButton';
+import Anchor from '../../components/authentication/Anchor';
+import Image from '../../components/authentication/Image';
 
-export const Login = () => (<div>Login view</div>);
+export const Login = () => (
+  <Wrapper>
+    <FormContent id="formContent">
+      <FormHeader>
+        <Image src={logo} alt="Calorify Logo" />
+        <h1>Calorify</h1>
+      </FormHeader>
+
+      <form>
+        <Input type="text" name="login" placeholder="email" />
+        <Input type="password" name="login" placeholder="password" />
+        <SubmitButton type="submit" value="Log In" />
+        <SecondaryAction>
+          <Anchor to="/auth/signup"> Create an account</Anchor>
+        </SecondaryAction>
+      </form>
+
+      <FormFooter id="formFooter">
+        <Anchor to="/auth/forgot-password"> Forgot Password?</Anchor>
+      </FormFooter>
+
+    </FormContent>
+  </Wrapper>
+);
+
+// Login.propTypes = {
+//   navigate: PropsTypes.func.isRequired,
+// };
 
 export default Login;
--- client/src/containers/authentication/Signup.js
+++ client/src/containers/authentication/Signup.js
@@ -1,5 +1,35 @@
 import React from 'react';
+import logo from '../../assets/img/logo.png';
+import Wrapper from '../../components/authentication/Wrapper';
+import FormHeader from '../../components/authentication/FormHeader';
+import FormContent from '../../components/authentication/FormContent';
+import SecondaryAction from '../../components/authentication/SecondaryAction';
+import Input from '../../components/authentication/Input';
+import SubmitButton from '../../components/authentication/SubmitButton';
+import Anchor from '../../components/authentication/Anchor';
+import Image from '../../components/authentication/Image';
 
-export const Signup = () => (<div>Signup view</div>);
+export const Signup = () => (
+  <Wrapper>
+    <FormContent id="formContent">
+      <FormHeader>
+        <Image src={logo} alt="Calorify Logo" />
+        <h1>Calorify</h1>
+      </FormHeader>
+
+      <form>
+        <Input type="text" name="signup" placeholder="full name" />
+        <Input type="text" name="signup" placeholder="email" />
+        <Input type="password" name="signup" placeholder="password" />
+        <Input type="password" name="signup" placeholder="repeat password" />
+        <SubmitButton type="submit" value="Sign Up" />
+        <SecondaryAction>
+          <Anchor to="/auth/login">Log in</Anchor>
+        </SecondaryAction>
+      </form>
+
+    </FormContent>
+  </Wrapper>
+);
 
 export default Signup;
