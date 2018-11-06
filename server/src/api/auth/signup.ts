180eabf212a884a0cde2617ba51ad8b3ee4fa626
Veselin Dimitrov <vidimi7rov@gmail.com>
2018-11-06 21:06:21 +0200

    Validate the email and find the user by email before signup

--- server/src/api/auth/signup.ts
+++ server/src/api/auth/signup.ts
@@ -2,17 +2,18 @@ import Koa from 'koa';
 import { omit, isEmpty } from 'lodash';
 import User, { UserType } from '../../models/user/User';
 import respondWith from '../../lib/respondWith';
+import { isValidEmail } from '../../lib/validations';
 
 const signup = async (ctx: Koa.Context) => {
   const attrs: UserType = (ctx.request.body as any).attrs;
   let user;
 
-  if (isEmpty(attrs)) {
+  if (isEmpty(attrs) || !isValidEmail(attrs.email)) {
     return respondWith.badRequest(ctx);
   }
 
   try {
-    user = await User.find(attrs);
+    user = await User.find({ email: attrs.email });
   } catch (err) {
     return respondWith.error(ctx, err);
   }
