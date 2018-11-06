ee221a8eda6ac7a9e29d7ebd86a8920c012521f6
Veselin Dimitrov <vidimi7rov@gmail.com>
2018-11-06 23:49:04 +0200

    Add AppBar to the main dashboard section

--- client/src/containers/dashboard/Main.js
+++ client/src/containers/dashboard/Main.js
@@ -1,24 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Redirect, navigate } from '@reach/router';
-import Button from '@material-ui/core/Button';
-import Wrapper from '../../components/dashboard/Wrapper';
-import Header from '../../components/dashboard/Header';
+import { Redirect } from '@reach/router';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MoreVert from '@material-ui/icons/MoreVert';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
 import Logo from '../../components/dashboard/Logo';
-import Headline from '../../components/dashboard/Headline';
-import logo from '../../assets/img/logo.png';
+import Wrapper from '../../components/dashboard/Wrapper';
+import CustomTypography from '../../components/dashboard/CustomTypography';
+import CustomIconButton from '../../components/dashboard/CustomIconButton';
 import { logout as logoutActionCreator } from '../../actions/authentication';
+import logo from '../../assets/img/logo.png';
 
 export class Main extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      anchorEl: null,
+    };
+
+    this.handleMenu = this.handleMenu.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleMenu(event) {
+    this.setState({
+      anchorEl: event.currentTarget,
+    });
+  }
+
+  handleClose() {
+    this.setState({
+      anchorEl: null,
+    });
   }
 
   render() {
-    const { user, logout } = this.props;
+    const {
+      user,
+      logout,
+    } = this.props;
+    const { navigate } = this.props;
+    const { anchorEl } = this.state;
+    const open = Boolean(anchorEl);
 
     if (!user) {
       return (
@@ -28,22 +58,51 @@ export class Main extends React.Component {
 
     return (
       <Wrapper>
-        <Header>
-          <Logo src={logo} />
-          <Headline>
-            Calorify - track your daily calories
-          </Headline>
-          <Button
-            variant="contained"
-            color="default"
-            onClick={() => {
-              logout()
-                .then(() => navigate('/auth/login'));
-            }}
-          >
-            Logout
-          </Button>
-        </Header>
+        <AppBar position="static">
+          <Toolbar>
+            <CustomIconButton color="secondary" aria-label="Menu">
+              <MenuIcon />
+            </CustomIconButton>
+            <Logo src={logo} alt="logo" />
+            <CustomTypography variant="h6" color="secondary">
+              Calorify
+            </CustomTypography>
+            <div>
+              <IconButton
+                aria-owns={open ? 'menu-appbar' : undefined}
+                aria-haspopup="true"
+                onClick={() => navigate('/settings')}
+                color="secondary"
+              >
+                <AccountCircle />
+              </IconButton>
+              <IconButton
+                aria-owns={open ? 'menu-appbar' : undefined}
+                aria-haspopup="true"
+                onClick={this.handleMenu}
+                color="secondary"
+              >
+                <MoreVert />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={this.handleClose}
+              >
+                <MenuItem onClick={() => logout().then(() => navigate('/auth/login'))}>Logout</MenuItem>
+              </Menu>
+            </div>
+          </Toolbar>
+        </AppBar>
       </Wrapper>
     );
   }
@@ -58,6 +117,7 @@ Main.propTypes = {
     provider: PropTypes.string.isRequired,
     daily_calories_limit: PropTypes.number.isRequired,
   }),
+  navigate: PropTypes.func.isRequired,
   logout: PropTypes.func.isRequired,
 };
 
