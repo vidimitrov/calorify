f589fa2e9ca6f59e014b082ed7dc73d45190abeb
Veselin Dimitrov <vidimi7rov@gmail.com>
2018-11-06 23:48:20 +0200

    Update existing styles

--- client/src/components/dashboard/Logo.js
+++ client/src/components/dashboard/Logo.js
@@ -1,20 +1,10 @@
-import styled, { keyframes } from 'styled-components';
-
-const rotate = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-
-  to {
-    transform: rotate(360deg);
-  }
-`;
+import styled from 'styled-components';
 
 const Logo = styled.img.attrs({
   alt: 'logo',
 })`
-  animation: ${rotate} infinite 20s linear;
-  height: 40vmin;
+  height: 25px;
+  margin-right: 10px;
   src: url(${props => props.src});
 `;
 
--- client/src/components/dashboard/Wrapper.js
+++ client/src/components/dashboard/Wrapper.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const Wrapper = styled.div`
   background-color: #ffffff;
   height: 100vh;
+  flex-grow: 1,
 `;
 
 export default Wrapper;
