141143be97f3372e6815e2fbc3325145b655294c
Veselin Dimitrov <vidimi7rov@gmail.com>
2018-11-06 23:48:39 +0200

    Add custom material-ui components

---
+++ client/src/components/dashboard/CustomIconButton.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import IconButton from '@material-ui/core/IconButton';
+
+const CustomIconButton = styled(IconButton)`
+  margin-left: -12px;
+  margin-right: 20px;
+`;
+
+export default CustomIconButton;
