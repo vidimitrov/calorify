1fe1a5ff6f2951bc8473927dedc4261f24182521
Veselin Dimitrov <vidimi7rov@gmail.com>
2018-11-07 18:09:13 +0200

    Create a FabButton component

---
+++ client/src/components/dashboard/FabButton.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
+
+const FabButton = styled(Button)`
+  && {
+    position: fixed;
+    bottom: 24px;
+    right: 24px;
+  }
+`;
+
+export default FabButton;
