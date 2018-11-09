5e8d4d24d24e949a8cf41b56cce78763dba8afff
Veselin Dimitrov <vidimi7rov@gmail.com>
2018-11-09 22:35:47 +0200

    Add user reducers tests

---
+++ client/src/reducers/users.test.js
@@ -0,0 +1,109 @@
+import reducer from './user';
+import {
+  STORE_USER_DATA,
+  UPDATE_USER_START,
+  UPDATE_USER_SUCCESS,
+  UPDATE_USER_FAILURE,
+} from '../constants/actionTypes';
+
+let initialState = {};
+
+describe('Users reducers', () => {
+  beforeEach(() => {
+    initialState = {
+      data: {},
+      loading: false,
+      error: null,
+    };
+  });
+
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual({
+      data: {},
+      loading: false,
+      error: null,
+    });
+  });
+
+  it('should handle STORE_USER_DATA action', () => {
+    const user = {
+      name: 'John Doe',
+      email: 'john@email.com',
+    };
+    const action = {
+      type: STORE_USER_DATA,
+      payload: user,
+    };
+
+    expect(reducer(initialState, action)).toEqual({
+      data: user,
+      loading: false,
+      error: null,
+    });
+  });
+
+  it('should handle UPDATE_USER_START action', () => {
+    const updates = {
+      name: 'John Doe Passos',
+    };
+    const action = {
+      type: UPDATE_USER_START,
+      payload: {
+        userId: 'some-fake-id',
+        updates,
+      },
+    };
+
+    expect(reducer(initialState, action)).toEqual({
+      data: {},
+      loading: true,
+      error: null,
+    });
+  });
+
+  it('should handle UPDATE_USER_SUCCESS action', () => {
+    const state = {
+      ...initialState,
+      data: {
+        name: 'John Doe',
+      },
+    };
+    const updatedUser = {
+      name: 'John Doe Passos',
+    };
+    const action = {
+      type: UPDATE_USER_SUCCESS,
+      payload: {
+        user: updatedUser,
+      },
+    };
+
+    expect(reducer(state, action)).toEqual({
+      data: {
+        name: updatedUser.name,
+      },
+      loading: false,
+      error: null,
+    });
+  });
+
+  it('should handle UPDATE_USER_FAILURE action', () => {
+    const error = {
+      success: false,
+      statusCode: 400,
+      message: 'Bad request',
+    };
+    const action = {
+      type: UPDATE_USER_FAILURE,
+      payload: {
+        error,
+      },
+    };
+
+    expect(reducer(initialState, action)).toEqual({
+      data: {},
+      loading: false,
+      error,
+    });
+  });
+});
