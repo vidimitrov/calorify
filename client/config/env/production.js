7f5f341183b40ab5bc64d50231c1188cdf681e34
Veselin Dimitrov <vidimi7rov@gmail.com>
2018-11-11 01:04:53 +0200

    Fix configuration loading. Add test config and add APP_URL entry

--- client/config/env/production.js
+++ client/config/env/production.js
@@ -1,3 +1,4 @@
 export default {
   API_URL: null,
+  APP_URL: 'http://clarify.jsoutof.space',
 };
--- client/config/index.js
+++ client/config/index.js
@@ -1,7 +1,22 @@
 import devCfg from './env/development';
+import testCfg from './env/test';
 import prodCfg from './env/production';
 
 const ENV = process.env.NODE_ENV;
-const config = ENV === 'development' ? devCfg : prodCfg;
+let config;
+
+switch (ENV) {
+  case 'development':
+    config = devCfg;
+    break;
+  case 'test':
+    config = testCfg;
+    break;
+  case 'production':
+    config = prodCfg;
+    break;
+  default:
+    break;
+}
 
 export default config;
