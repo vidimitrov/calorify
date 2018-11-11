e0435cd96a8da6f7152b750b2ecbb909b4984d07
Veselin Dimitrov <vidimi7rov@gmail.com>
2018-11-12 00:48:39 +0200

    Add invalid styles for the InputWrapper component

--- client/src/components/dashboard/InputWrapper.js
+++ client/src/components/dashboard/InputWrapper.js
@@ -1,7 +1,11 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const InputWrapper = styled.div`
   margin-bottom: 20px;
+
+  ${props => props.invalid && css`
+    margin-bottom: 5px
+  `}}
 `;
 
 export default InputWrapper;
