8910c759c05b4c1e8198d7bcf5ff9192f7066547
Veselin Dimitrov <vidimi7rov@gmail.com>
2018-11-12 23:52:33 +0200

    Add filters to user's meals list

--- client/src/containers/dashboard/admin/Meals.js
+++ client/src/containers/dashboard/admin/Meals.js
@@ -7,7 +7,11 @@ import _ from 'lodash';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Snackbar from '@material-ui/core/Snackbar';
+import TextField from '@material-ui/core/TextField';
+import IconButton from '@material-ui/core/IconButton';
 import ArrowBack from '@material-ui/icons/ArrowBack';
+import FilterList from '@material-ui/icons/FilterList';
+import Close from '@material-ui/icons/Close';
 import AddIcon from '@material-ui/icons/Add';
 import CustomTypography from '../../../components/dashboard/CustomTypography';
 import CustomIconButton from '../../../components/dashboard/CustomIconButton';
@@ -23,22 +27,40 @@ import CardInfo from '../../../components/dashboard/Card/CardInfo';
 import CardDate from '../../../components/dashboard/Card/CardDate';
 import CardActions from '../../../components/dashboard/Card/CardActions';
 import CardStatus from '../../../components/dashboard/Card/CardStatus';
+import Filters from '../../../components/dashboard/Filters';
+import FiltersControls from '../../../components/dashboard/FiltersControls';
 import logo from '../../../assets/img/logo.png';
 import {
   fetchMeals as fetchMealsActionCreator,
+  removeMeal as removeMealActionCreator,
+  filterMeals as filterMealsAction,
+  resetMealsFilters as resetMealsFiltersAction,
 } from '../../../actions/meals';
 
 export class Meals extends React.Component {
   constructor(props) {
     super(props);
 
+    const { account } = props;
+    const { createdAt } = account || {};
+
     this.state = {
+      anchorEl: null,
+      showFilters: false,
+      dateFrom: moment(createdAt).toISOString().split('T')[0],
+      dateTo: moment().toISOString().split('T')[0],
+      timeFrom: '00:00',
+      timeTo: '23:59',
       positiveSnackbarOpen: false,
       negativeSnackbarOpen: false,
       positiveMessage: null,
       negativeMessage: null,
     };
 
+    this.handleFilterChange = this.handleFilterChange.bind(this);
+    this.toggleFiltersVisibility = this.toggleFiltersVisibility.bind(this);
+    this.isInRange = this.isInRange.bind(this);
+    this.getTotalCaloriesForDate = this.getTotalCaloriesForDate.bind(this);
     this.handlePositiveSnackbarClose = this.handlePositiveSnackbarClose.bind(this);
     this.handleNegativeSnackbarClose = this.handleNegativeSnackbarClose.bind(this);
   }
@@ -93,20 +115,75 @@ export class Meals extends React.Component {
     });
   }
 
+  toggleFiltersVisibility() {
+    const { account, resetMealsFilters } = this.props;
+    const { showFilters } = this.state;
+    let updates = {};
+
+    if (showFilters) {
+      resetMealsFilters();
+      updates = {
+        dateFrom: moment(account.createdAt).toISOString().split('T')[0],
+        dateTo: moment().toISOString().split('T')[0],
+        timeFrom: '00:00',
+        timeTo: '23:59',
+      };
+    }
+
+    this.setState({
+      ...updates,
+      showFilters: !showFilters,
+    });
+  }
+
+  handleFilterChange(key, value) {
+    const {
+      filterMeals,
+    } = this.props;
+
+    this.setState({
+      [key]: value,
+    }, () => {
+      const {
+        dateFrom,
+        dateTo,
+        timeFrom,
+        timeTo,
+      } = this.state;
+
+      filterMeals({
+        dateFrom,
+        dateTo,
+        timeFrom,
+        timeTo,
+      });
+    });
+  }
+
   render() {
     const {
       account,
       navigate,
-      meals,
       removeMeal,
+      filteredMeals,
     } = this.props;
     const {
+      anchorEl,
       positiveSnackbarOpen,
       negativeSnackbarOpen,
       positiveMessage,
       negativeMessage,
+      showFilters,
+      dateFrom,
+      dateTo,
+      timeFrom,
+      timeTo,
     } = this.state;
+    let { meals } = this.props;
 
+    if (showFilters) {
+      meals = filteredMeals;
+    }
 
     if (!account) {
       return (
@@ -118,7 +195,7 @@ export class Meals extends React.Component {
       );
     }
 
-
+    const open = Boolean(anchorEl);
     const mealsGroupedByDate = _.groupBy(meals, 'date');
     const groupDates = Object.keys(mealsGroupedByDate).sort((a, b) => moment(b).diff(moment(a)));
     /* eslint react/no-array-index-key: 0 */
@@ -136,6 +213,58 @@ export class Meals extends React.Component {
             </CustomTypography>
           </Toolbar>
         </AppBar>
+        <Filters visible={showFilters}>
+          <FiltersControls visible={showFilters}>
+            <TextField
+              label="From"
+              type="date"
+              value={dateFrom}
+              onChange={e => this.handleFilterChange('dateFrom', e.target.value)}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+            <TextField
+              label="To"
+              type="date"
+              value={dateTo}
+              onChange={e => this.handleFilterChange('dateTo', e.target.value)}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+            <TextField
+              label="From"
+              type="time"
+              value={timeFrom}
+              onChange={e => this.handleFilterChange('timeFrom', e.target.value)}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+            <TextField
+              label="To"
+              type="time"
+              value={timeTo}
+              onChange={e => this.handleFilterChange('timeTo', e.target.value)}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </FiltersControls>
+          <div>
+            <IconButton
+              aria-owns={open ? 'menu-appbar' : undefined}
+              aria-haspopup="true"
+              onClick={() => {
+                this.toggleFiltersVisibility();
+              }}
+              color="default"
+            >
+              {!showFilters ? <FilterList /> : <Close />}
+            </IconButton>
+          </div>
+        </Filters>
         <CustomList>
           <ScrollContainer>
             {groupDates.length > 0 && groupDates.map((gDate, index) => (
@@ -182,7 +311,7 @@ export class Meals extends React.Component {
                   ))}
               </div>
             ))}
-            {groupDates.length === 0
+            {groupDates.length === 0 && !showFilters
               && <h3>User has no meals</h3>
             }
           </ScrollContainer>
@@ -248,23 +377,37 @@ Meals.propTypes = {
     time: PropTypes.string.isRequired,
     calories: PropTypes.number.isRequired,
   })),
+  filteredMeals: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    time: PropTypes.string.isRequired,
+    calories: PropTypes.number.isRequired,
+  })),
   navigate: PropTypes.func.isRequired,
   removeMeal: PropTypes.func.isRequired,
   getAllMealsForUser: PropTypes.func.isRequired,
+  filterMeals: PropTypes.func.isRequired,
+  resetMealsFilters: PropTypes.func.isRequired,
 };
 
 Meals.defaultProps = {
   account: null,
   meals: [],
+  filteredMeals: [],
 };
 
 const mapStateToProps = state => ({
   account: _.isEmpty(state.account.data) ? null : state.account.data,
   meals: state.meals.data,
+  filteredMeals: state.meals.filteredData,
 });
 
 const mapDispatchToProps = dispatch => ({
   getAllMealsForUser: userId => dispatch(fetchMealsActionCreator(userId)),
+  removeMeal: mealId => dispatch(removeMealActionCreator(mealId)),
+  filterMeals: filters => dispatch(filterMealsAction(filters)),
+  resetMealsFilters: () => dispatch(resetMealsFiltersAction()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Meals);
