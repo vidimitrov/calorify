dcfc2b1e76ffeec3916d1b4e45299614494bc8ca
Veselin Dimitrov <vidimi7rov@gmail.com>
2018-11-12 18:32:45 +0200

    Add create, list, remove API functions

--- client/src/api/users.js
+++ client/src/api/users.js
@@ -13,6 +13,45 @@ export const parseUser = user => ({
   updatedAt: user.updated_at,
 });
 
+export const create = async (token, userData) => {
+  const response = await fetch(`${API_URL}/api/users`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      attrs: userData,
+    }),
+  });
+  const data = await response.json();
+
+  if (data.success) {
+    const user = parseUser(data.user);
+    return Promise.resolve({ user });
+  }
+
+  return Promise.reject(data);
+};
+
+export const list = async (token) => {
+  const response = await fetch(`${API_URL}/api/users`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const data = await response.json();
+
+  if (data.success) {
+    const users = data.users.map(user => (parseUser(user)));
+    return Promise.resolve({ users });
+  }
+
+  return Promise.reject(data);
+};
+
 export const update = async (token, userId, updates) => {
   const response = await fetch(`${API_URL}/api/users/${userId}`, {
     method: 'PATCH',
@@ -26,6 +65,24 @@ export const update = async (token, userId, updates) => {
         daily_calories_limit: updates.dailyCalories,
       },
     }),
+  });
+  const data = await response.json();
+
+  if (data.success) {
+    const user = parseUser(data.user);
+    return Promise.resolve({ user });
+  }
+
+  return Promise.reject(data);
+};
+
+export const remove = async (token, userId) => {
+  const response = await fetch(`${API_URL}/api/users/${userId}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
   });
   const data = await response.json();
 
