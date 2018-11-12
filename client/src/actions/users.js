990679518d7d4769269c855015932d9dcd50333b
Veselin Dimitrov <vidimi7rov@gmail.com>
2018-11-12 22:05:44 +0200

    Add all users actions

--- client/src/actions/users.js
+++ client/src/actions/users.js
@@ -1,4 +1,9 @@
-import { update } from '../api/users';
+import {
+  create,
+  list,
+  update,
+  remove,
+} from '../api/users';
 import {
   FETCH_USERS_START,
   FETCH_USERS_SUCCESS,
@@ -18,6 +23,93 @@ import {
   updateAccountSuccess,
   updateAccountFailure,
 } from './account';
+
+/**
+ *  Create user actions
+ *  */
+export const createUserStart = user => ({
+  type: CREATE_USER_START,
+  payload: { user },
+});
+
+export const createUserSuccess = user => ({
+  type: CREATE_USER_SUCCESS,
+  payload: { user },
+});
+
+export const createUserFailure = error => ({
+  type: CREATE_USER_FAILURE,
+  payload: { error },
+});
+
+/**
+ * Create user action response handlers
+ *  */
+const handleCreateUserSuccess = dispatch => (response) => {
+  const { user } = response;
+  dispatch(createUserSuccess(user));
+  return Promise.resolve(user);
+};
+const handleCreateUserFailure = dispatch => (error) => {
+  dispatch(createUserFailure(error));
+  return Promise.reject(error);
+};
+
+/**
+ *  Create user action creator
+ *  */
+export const createUser = user => (dispatch, getState) => {
+  dispatch(createUserStart(user));
+  const state = getState();
+  const { token } = state.auth;
+
+  return create(token, user)
+    .then(handleCreateUserSuccess(dispatch))
+    .catch(handleCreateUserFailure(dispatch));
+};
+
+/**
+ *  Fetch users actions
+ *  */
+export const fetchUsersStart = () => ({
+  type: FETCH_USERS_START,
+});
+
+export const fetchUsersSuccess = users => ({
+  type: FETCH_USERS_SUCCESS,
+  payload: { users },
+});
+
+export const fetchUsersFailure = error => ({
+  type: FETCH_USERS_FAILURE,
+  payload: { error },
+});
+
+/**
+ * Fetch users action response handlers
+ *  */
+const handleFetchUsersSuccess = dispatch => (response) => {
+  const { users } = response;
+  dispatch(fetchUsersSuccess(users));
+  return Promise.resolve(users);
+};
+const handleFetchUsersFailure = dispatch => (error) => {
+  dispatch(fetchUsersFailure(error));
+  return Promise.reject(error);
+};
+
+/**
+ *  Fetch users action creator
+ *  */
+export const fetchUsers = () => (dispatch, getState) => {
+  dispatch(fetchUsersStart());
+  const state = getState();
+  const { token } = state.auth;
+
+  return list(token)
+    .then(handleFetchUsersSuccess(dispatch))
+    .catch(handleFetchUsersFailure(dispatch));
+};
 
 /**
  *  Update user actions
@@ -81,4 +173,48 @@ export const updateUser = (userId, updates) => (dispatch, getState) => {
   return update(token, userId, updates)
     .then(handleUpdateUserSuccess(dispatch, state))
     .catch(handleUpdateUserFailure(dispatch, state));
+};
+
+/**
+ *  Remove user actions
+ *  */
+export const removeUserStart = userId => ({
+  type: REMOVE_USER_START,
+  payload: { userId },
+});
+
+export const removeUserSuccess = user => ({
+  type: REMOVE_USER_SUCCESS,
+  payload: { user },
+});
+
+export const removeUserFailure = error => ({
+  type: REMOVE_USER_FAILURE,
+  payload: { error },
+});
+
+/**
+ * Remove user action response handlers
+ *  */
+const handleRemoveUserSuccess = dispatch => (response) => {
+  const { user } = response;
+  dispatch(removeUserSuccess(user));
+  return Promise.resolve(user);
+};
+const handleRemoveUserFailure = dispatch => (error) => {
+  dispatch(removeUserFailure(error));
+  return Promise.reject(error);
+};
+
+/**
+ *  Remove user action creator
+ *  */
+export const removeUser = userId => (dispatch, getState) => {
+  dispatch(removeUserStart(userId));
+  const state = getState();
+  const { token } = state.auth;
+
+  return remove(token, userId)
+    .then(handleRemoveUserSuccess(dispatch))
+    .catch(handleRemoveUserFailure(dispatch));
 };
